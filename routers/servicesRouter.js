9f5a30726b463f2c7d8471b1dbbf9ed16628e0b5
Dragos Bahrim <dragosbahrim@outlook.com>
2022-03-24 21:29:24 +0200

    Update services after button is pressed

--- routers/servicesRouter.js
+++ routers/servicesRouter.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/increase/:idService/', authController.protect, servicesController.upgradeService)
+router.get('/:idService/', authController.protect, servicesController.getService)
 
 module.exports = router;
